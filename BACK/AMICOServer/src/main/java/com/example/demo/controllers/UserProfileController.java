f9a9de141a8ef5215d282d53c4434973990242dc
Carlos Gil Herranz <Carlosghms@users.noreply.github.com>
2018-02-17 03:27:51 +0100

    UserProfileController Created
    New controller and some changes to userprofile.html

---
+++ BACK/AMICOServer/src/main/java/com/example/demo/controllers/UserProfileController.java
@@ -0,0 +1,34 @@
+
+package com.example.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.user_package.User;
+
+@Controller
+public class UserProfileController {
+
+	@RequestMapping("/profile/{id}")
+	public String viewProfile(Model model, User user) {
+
+		model.addAttribute("name", user.getUserFirstName());
+		model.addAttribute("lastName", user.getUserLastName());
+		model.addAttribute("userName", user.getUsername());
+		model.addAttribute("mail", user.getUserMail());
+		model.addAttribute("address", user.getUserAddress());
+		model.addAttribute("city", user.getCity());
+		model.addAttribute("country", user.getCountry());
+		model.addAttribute("phoneNumber", user.getPhoneNumber());
+		model.addAttribute("urlImage", user.getUrlProfileImage());
+		model.addAttribute("inscribedCourses", user.getInscribedCourses());
+		model.addAttribute("completedCourses", user.getCompletedCourses());
+		model.addAttribute("urlImage", user.getUrlProfileImage());
+		model.addAttribute("role", user.getRole());
+		model.addAttribute("isStudent", user.isStudent());
+		
+
+		return "userProfile";
+	}
+}
