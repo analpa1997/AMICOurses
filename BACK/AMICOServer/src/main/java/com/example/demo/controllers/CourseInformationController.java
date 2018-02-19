00a70df5bddb452da1034eccdbeeadd87acda425
analpa1997 <a.alcojor@alumnos.urjc.es>
2018-02-19 11:43:10 +0100

    Tried to connect Subjects to Course

--- BACK/AMICOServer/src/main/java/com/example/demo/controllers/CourseInformationController.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/controllers/CourseInformationController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controllers;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,55 +9,51 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.demo.subject_package.Subject;
-import com.example.demo.subject_package.SubjectRepository;
 import com.example.demo.course_package.Course;
 import com.example.demo.course_package.CourseRepository;
+import com.example.demo.subject_package.Subject;
+import com.example.demo.subject_package.SubjectRepository;
 
 @Controller
 public class CourseInformationController {
-	
+
 	private Course course;
-			
+
 	@Autowired
 	private SubjectRepository sujectRepository;
 	@Autowired
 	private CourseRepository courseRepository;
-	
-	
+
 	@RequestMapping("/course/{courseID}")
 	public String course(Model model, @PathVariable long courseID) {
-		
+
 		System.out.println("course" + courseID);
 		course = courseRepository.findOne(courseID);
-			
+
 		model.addAttribute("courseDescription", course.getCourseDescription());
 		return "HTML/courseInformation/course";
 	}
 
-	@RequestMapping("/subjects/{courseID}")
-	public String subjects(Model model,  @PathVariable long courseID) {
-		
-		List<String> sName = new ArrayList<> ();
-		
-		course = courseRepository.findOne(courseID);
+	@RequestMapping("/subjects/{internalName}")
+	public String subjects(Model model, @PathVariable String internalName) {
+
+		List<String> sName = new ArrayList<>();
+
+		course = courseRepository.findByInternalName(internalName);
 		List<Subject> subject = course.getSubjects();
-		
+
 		/*
-		 Iterator<Subject> iter = subjects.iterator();
-		 
-		while (iter.hasNext()) {
-			Subject s = new Subject();
-			s=iter.next();
-			sName.add(s.getName());
-		}
-		*/
+		 * Iterator<Subject> iter = subjects.iterator();
+		 * 
+		 * while (iter.hasNext()) { Subject s = new Subject(); s=iter.next();
+		 * sName.add(s.getName()); }
+		 */
 		System.out.println("# Subject" + " " + subject.size());
-		
+
 		model.addAttribute("subjects", subject);
-		
-		
+		model.addAttribute("courseName", internalName);
+
 		return "HTML/CourseInformation/subjects";
 	}
-	
+
 }
