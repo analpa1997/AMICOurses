ef331db5357df612f44788fea9aaea249b67e694
mest96 <mestilles96@gmail.com>
2018-02-23 15:53:11 +0100

    courseInformation

--- BACK/AMICOServer/src/main/java/com/example/demo/controllers/CourseInformationController.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/controllers/CourseInformationController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.text.SimpleDateFormat;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,11 +8,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.course_package.Course;
 import com.example.demo.course_package.CourseRepository;
 import com.example.demo.skill_package.Skill;
 import com.example.demo.subject_package.Subject;
+import com.example.demo.user_package.UserRepository;
+import com.example.demo.user_package.User;
+
 
 @Controller
 public class CourseInformationController {
@@ -20,6 +25,10 @@ public class CourseInformationController {
 
 	@Autowired
 	private CourseRepository courseRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+	
 
 	// For course main page description
 	@RequestMapping("/course/{internalName}/{userName}")
@@ -80,5 +89,35 @@ public class CourseInformationController {
 
 		return "HTML/CourseInformation/skills";
 	}
+	
+	@RequestMapping("/course/{internalName}/{userName}/add")
+	public ModelAndView addCourseToUser(Model model, 
+			@PathVariable String internalName,
+			@PathVariable String userName) {
+
+		User user = userRepository.findByUsername(userName);
+
+		System.out.println("# cursos inscritos" + " " + user.getInscribedCourses().size());
+		for (int i=0; i<user.getInscribedCourses().size(); i++)
+			System.out.println("# nombre" + " " + user.getInscribedCourses().get(i).getName());
+		
+		for (User u : userRepository.findAll()) {
+			if (!u.getInscribedCourses().contains(course))
+						
+			System.out.println("course : " + course.getName() + " no lo tiene");
+			
+			user.getInscribedCourses().add(course);			
+			user.setInscribedCourses(user.getInscribedCourses());
+			System.out.println("# cursos inscritos" + " " + user.getInscribedCourses().size());
+			
+			user.setInscribedCourses(user.getInscribedCourses());
+			
+			userRepository.save(user);
+		}
+		
+		return new ModelAndView("redirect:/profile/"+ user.getUsername());
+	}
+	
+	
 
 }
