ead2ca6291a297515a6fe3f4cbd13f63c6f02ea7
mest96 <mestilles96@gmail.com>
2018-02-25 13:56:15 +0100

    courseInformation
    Añadido controlar que el curso selecionado, no esta incluido en completed

--- BACK/AMICOServer/src/main/java/com/example/demo/controllers/CourseInformationController.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/controllers/CourseInformationController.java
@@ -25,7 +25,9 @@ public class CourseInformationController {
 
 	private Course course = null;
 	private String userName;
-
+	private User usr = null;
+	private boolean isAnStudent=false;
+	
 	@Autowired
 	private CourseRepository courseRepository;
 
@@ -40,21 +42,19 @@ public class CourseInformationController {
 	// For course main page description
 	@RequestMapping("/course/{internalName}")
 	public String course(Model model, @PathVariable String internalName) {
-		
-		User u = null;
-		
+				
 		// Check for the logged user
-		u = sessionUserComponent.getLoggedUser();
-		if (u == null)
-			userName = "amico";  // para que funcione
-		else 
-			userName = u.getInternalName();
+		usr = sessionUserComponent.getLoggedUser();
 		
+		if (usr == null)
+			userName = "amico";  // para que funcione
+		else {
+			userName = usr.getInternalName();
+			isAnStudent = usr.isStudent();
+		}
 		// get the course information by course internal name
 		course = courseRepository.findByInternalName(internalName);
-		System.out.println(internalName);
-		System.out.println(userName);
-
+		
 		// set the course  attributes to show
 		model.addAttribute("courseName", course.getName());
 		model.addAttribute("courseDescription", course.getCourseDescription());
@@ -66,7 +66,6 @@ public class CourseInformationController {
 		model.addAttribute("startDateString", startDateString);
 		model.addAttribute("endDateString", endDateString);
 		model.addAttribute("urlImage", course.getUrlImage());
-		//model.addAttribute("nameInternal", internalName);
 
 		return "HTML/courseInformation/course";
 	}
@@ -105,89 +104,78 @@ public class CourseInformationController {
 	@RequestMapping("/course/{internalName}/add")
 	public ModelAndView addCourseToUser(Model model, @PathVariable String internalName) {
 
-		int courseCounter;
-		List<User> u = null;
-		List<Course> c = null;
+		int courseInscribedCounter;
+		int courseCompletedCounter;
+		List<User> uInscribedList = null;   // Users list
+		List<Course> cInscribedUsers = null; // Courses list
+		List<Course> cCompletedList = null; 
+		ModelAndView mv = null;
+				
 		
-		if (userName.equals("anonymous"))
-			return new ModelAndView("redirect:/");
-		
-		User user = userRepository.findByUsername(userName); // get all user information by userName
-		if (course.getInscribedUsers().size() > 0)
-			u = course.getInscribedUsers(); // get list of user subscribed to course
-		else
-/***** HEAD  
-			u = new ArrayList <>(); // initialize array to add user
-		
-		if (user.getInscribedCourses().size() > 0)
-       	 	c = user.getInscribedCourses(); // get the list of courses to which the user is subscribed
-		else 
-			c = new ArrayList <>(); // initialize array to add users
-		
-       	System.out.println("# cursos inscritos" + " " + c.size());
-       	System.out.println("# usuarios del curso" + " " + course.getInscribedUsers().size());
-       	
-       	// Check if user is already subscribed to the course
-       	Iterator<Course> itr = c.iterator();
-        courseCounter = 0;
-        while(itr.hasNext() ) {
-           Course cr = itr.next();
-           System.out.println(" curso " + " " + course.getCourseID() + " cursoIterator " + "" + cr.getCourseID());
-           if (cr.getCourseID() == course.getCourseID()) {
-        	      courseCounter ++;
-           }
-        }
-        System.out.println("courseCounter : " + courseCounter);	
-        // if user not subscribed to the course, inscribe him
-        	if (courseCounter != 1) {
-        		 	   						
-			System.out.println("course : " + course.getName() + " No lo tiene");		
-			
-			c.add(course);			
-END HEAD ***/
-			u = new ArrayList<>(); // initialize array to add user
-
-		if (user.getInscribedCourses().size() > 0)
-			c = user.getInscribedCourses(); // get the list of courses to which the user is subscribed
-		else
-			c = new ArrayList<>(); // initialize array to add users
-
-		System.out.println("# cursos inscritos" + " " + c.size());
-		System.out.println("# usuarios del curso" + " " + course.getInscribedUsers().size());
-
-		// Check if user is already subscribed to the course
-		Iterator<Course> itr = c.iterator();
-		courseCounter = 0;
-		while (itr.hasNext()) {
-			Course cr = itr.next();
-			System.out.println(" curso " + " " + course.getCourseID() + " cursoIterator " + "" + cr.getCourseID());
-			if (cr.getCourseID() == course.getCourseID()) {
-				courseCounter++;
+		// Check the users to allow or not the course registration 
+		if (userName.equals("anonymous")) {
+			//"You must be logged-in to subscribe to a course. Return to main page and log-in");
+			// return to /course/{internalName}
+		}
+		else {
+			if (!isAnStudent) {
+				// "Teachers cannot register in courses"
+				// return to /course/{internalName}
 			}
 		}
-		System.out.println("courseCounter : " + courseCounter);
+
+		
+		User user = userRepository.findByUsername(userName); // get all user information by userName
+		
+		if (course.getInscribedUsers().size() > 0)
+			uInscribedList = course.getInscribedUsers(); // get list of user subscribed to course
+		else
+			uInscribedList = new ArrayList<>(); // initialize array to add user
+
+		if (user.getInscribedCourses().size() > 0)
+			cInscribedUsers = user.getInscribedCourses(); // get the list of courses to which the user is subscribed
+		else
+			cInscribedUsers = new ArrayList<>(); // initialize array to add users
+		
+		if (user.getCompletedCourses().size() > 0)
+			cCompletedList = user.getCompletedCourses(); // get the list of courses to which the user is subscribed
+		else
+			cCompletedList = new ArrayList<>(); // initialize array to add users
+		
+		System.out.println("# cursos inscritos" + " " + cInscribedUsers.size());
+
+		courseInscribedCounter = 0;
+		while (courseInscribedCounter < cInscribedUsers.size() && cInscribedUsers.get(courseInscribedCounter).getCourseID() != course.getCourseID())
+			courseInscribedCounter++;
+		
+		System.out.println("courseCounter : " + courseInscribedCounter);
+		
+		courseCompletedCounter = 0;
+		while (courseCompletedCounter < cCompletedList.size() && cCompletedList.get(courseCompletedCounter).getCourseID() != course.getCourseID())
+			courseCompletedCounter++;
+		
+		System.out.println("courseCounter : " + courseCompletedCounter);
+		
 		// if user not subscribed to the course, register him
-		if (courseCounter != 1) {
+		if (courseInscribedCounter == cInscribedUsers.size() && courseCompletedCounter == cCompletedList.size()) {
 
 			System.out.println("course : " + course.getName() + " No lo tiene");
 
-			c.add(course);
-/*** >>>>>>> 19411b2f2870148992f19ee82443e327540cfa06 ***/
-			user.setInscribedCourses(c);
+			cInscribedUsers.add(course);
+			user.setInscribedCourses(cInscribedUsers);
 			userRepository.save(user); // update user
 
-			u.add(user);
-			course.setInscribedUsers(u);
+			uInscribedList.add(user);
+			course.setInscribedUsers(uInscribedList);
 			course.setNumberOfUsers(course.getNumberOfUsers() + 1);
 			courseRepository.save(course);// update courses
+			return new ModelAndView("redirect:/profile/" + user.getUsername());
 
 		}
-
-		user = userRepository.findByUsername(userName);
-		System.out.println("# cursos inscritos" + " " + user.getInscribedCourses().size());
-		
-		return new ModelAndView("redirect:/profile/" + user.getUsername());
-		
+		else {//" already registered in this course");
+			return new ModelAndView("redirect:/course/{internalName}/").addObject("error", "You are already registered in this course"); 
+			
+		}
 	}
 
 }
