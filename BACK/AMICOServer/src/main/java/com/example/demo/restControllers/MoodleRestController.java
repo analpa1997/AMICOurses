69cc475a5a2bc801d62f937d47692d29b259a0d8
ivanchi88 <ivanmotos_8@hotmail.es>
2018-03-06 10:41:19 +0100

    Added some rest controller headers to be implemented at the MoodleRestController

--- BACK/AMICOServer/src/main/java/com/example/demo/restControllers/MoodleRestController.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/restControllers/MoodleRestController.java
@@ -13,7 +13,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.course.Course;
 import com.example.demo.course.CourseRepository;
@@ -31,8 +33,72 @@ public class MoodleRestController {
 
 	@Autowired
 	private SessionUserComponent sessionUserComponent;
-
-	@RequestMapping(value = "/api/moodle/getMarks/{courseInternalName}/{subjectInternalName}/practices/", method = RequestMethod.GET)
+	
+	/* MODULES */
+	/* POST */
+	/* Creates a new module within a subject */
+	@RequestMapping(value = "/api/moodle/{courseInternalName}/{subjectInternalName}/module", method = RequestMethod.POST)
+	public Object addModule (@PathVariable String courseInternalName, @PathVariable String subjectInternalNames) {
+	
+		return null;
+	}
+	
+	/* DELETE */
+	/* Creates a new module within a subject */
+	@RequestMapping(value = "/api/moodle/{courseInternalName}/{subjectInternalName}/module/{module}", method = RequestMethod.DELETE)
+	public Object deleteModule (@PathVariable String courseInternalName, @PathVariable String subjectInternalName, @PathVariable Integer module) {
+	
+		return null;
+	}
+	
+	/* STUDY ITEMS */
+	
+	/* GET */
+	/* Retrieves all the studyItems from a subject */
+	@RequestMapping(value = "/api/moodle/{courseInternalName}/{subjectInternalName}/studyItem/all", method = RequestMethod.GET)
+	public Object getAllStudyItems (@PathVariable String courseInternalName, @PathVariable String subjectInternalName) {
+	
+		return null;
+	}
+	
+	/* Retrieves the studyItems from a module from a subject */
+	@RequestMapping(value = "/api/moodle/{courseInternalName}/{subjectInternalName}/studyItem/module/{module}", method = RequestMethod.GET)
+	public Object getStudyItemsFromModule (@PathVariable String courseInternalName, @PathVariable String subjectInternalName, @PathVariable Integer module) {
+	
+		return null;
+	}
+	
+	/* Retrieves only one studyItem from a subject */
+	@RequestMapping(value = "/api/moodle/{courseInternalName}/{subjectInternalName}/studyItem/one/{studyItemID}", method = RequestMethod.GET)
+	public Object getOneStudyItem (@PathVariable String courseInternalName, @PathVariable String subjectInternalName, @PathVariable Long studyItemID) {
+	
+		return null;
+	}
+	
+	/* POST */
+	/* Creates a studyItem within a subject and a module*/
+	@RequestMapping(value = "/api/moodle/{courseInternalName}/{subjectInternalName}/studyItem/module/{module}", method = RequestMethod.POST)
+	public Object createStudyItem (@PathVariable String courseInternalName, @PathVariable String subjectInternalName, @PathVariable Integer module, @RequestParam String itemName,
+			@RequestParam String itemType, @RequestParam("itemFile") MultipartFile file) {
+	
+		return null;
+	}
+	
+	/* DELETE */
+	
+	
+	/*  */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/* TO DO: refractor all this autentication and checkings into a service*/
+	@RequestMapping(value = "/api/moodle/practicesMarks/{courseInternalName}/{subjectInternalName}/practices/", method = RequestMethod.GET)
 	public ResponseEntity<List<List<Object>>> allCourses(@PathVariable String courseInternalName,
 			@PathVariable String subjectInternalName) {
 
@@ -96,5 +162,5 @@ public class MoodleRestController {
 		/* If program reaches here is that there has been an error*/
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-
+	
 }
