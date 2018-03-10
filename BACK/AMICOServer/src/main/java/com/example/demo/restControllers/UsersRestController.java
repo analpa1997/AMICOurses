3b1dced8386d743561d0fa58602a53e715fd2fdb
Carlos Gil Herranz <Carlosghms@users.noreply.github.com>
2018-03-10 19:43:52 +0100

    UserRestController updated

--- BACK/AMICOServer/src/main/java/com/example/demo/restControllers/UsersRestController.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/restControllers/UsersRestController.java
@@ -6,8 +6,10 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.course.Course;
 import com.example.demo.user.User;
 import com.example.demo.user.UserRepository;
 
@@ -19,42 +21,56 @@ public class UsersRestController { // *** /!\ IN CONSTRUCTION, FOR YOUR SAFETY, 
 
 	// ********************** POST ********************
 	@RequestMapping(value = "/api/users/new", method = RequestMethod.POST)
-	public ResponseEntity<User> newUser(@PathVariable String userInternalName) {
-		return null;
-	}
+	public ResponseEntity<User> newUser(@RequestParam String username, @RequestParam String password,
+			@RequestParam String userMail, @RequestParam boolean isStudent) {
+		User user = new User(username, password, userMail, isStudent);
 
-	// ********************** FIND ********************
+		/* comprobar parametros correctos */
 
-	// Find user by internalName
-	@RequestMapping(value = "/api/users/{userInternalName}", method = RequestMethod.GET)
-	public ResponseEntity<User> findUser(@PathVariable String userInternalName) {
-		User user = repository.findByInternalName(userInternalName);
+		repository.save(user);
 		if (user != null)
 			return new ResponseEntity<>(user, HttpStatus.OK);
 		else
 			return new ResponseEntity(HttpStatus.NOT_FOUND);
 	}
 
-	// Find teacher by internalName
-	@RequestMapping(value = "/api/teachers/{userInternalName}", method = RequestMethod.GET)
-	public ResponseEntity<User> findTeacher(@PathVariable String userInternalName) {
+	// ********************** FIND ********************
+
+	// Find user by internalName
+	@RequestMapping(value = "/api/users/{userInternalName}", method = RequestMethod.GET)
+	public ResponseEntity<User> findUser(@PathVariable String userInternalName, @RequestParam boolean isStudent) {// null
+																													// to
+																													// anyone
 		User user = repository.findByInternalName(userInternalName);
-		if ((user != null) && (!user.isStudent()))
-			return new ResponseEntity<>(user, HttpStatus.OK);
+		if (user != null)
+			if (user.isStudent() == isStudent)// Student or Teacher
+				return new ResponseEntity<>(user, HttpStatus.OK);
+			else // isStudent==null-> AnyOne
+				return new ResponseEntity<>(user, HttpStatus.OK);
 		else
 			return new ResponseEntity(HttpStatus.NOT_FOUND);
 	}
 
-	// Find student by internalName
-	@RequestMapping(value = "/api/students/{userInternalName}", method = RequestMethod.GET)
-	public ResponseEntity<User> findStudent(@PathVariable String userInternalName) {
+	// ********************** FIND IN COURSE ********************
+	@RequestMapping(value = "/api/courses/{courseID}/{userInternalName}", method = RequestMethod.GET)
+	public ResponseEntity<User> findUserInCourse(@PathVariable String userInternalName, @PathVariable Long courseID) {
 		User user = repository.findByInternalName(userInternalName);
-		if ((user != null) && (user.isStudent()))
-			return new ResponseEntity<>(user, HttpStatus.OK);
-		else
+		if (user != null) {
+			for (Course c : user.getInscribedCourses())
+				if (c.getCourseID() == courseID)
+					return new ResponseEntity<>(user, HttpStatus.OK);
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		} else
 			return new ResponseEntity(HttpStatus.NOT_FOUND);
 	}
 
-	// User in course, all user, all teachers, all students, all teacher
+	// ********************** RETURN ALL ********************
+	/*
+	 * @RequestMapping(value = "/api/users/all", method = RequestMethod.GET) public
+	 * ResponseEntity<Page<User>> allUsers(Pageable pages){ Page<User> pageUser =
+	 * repository.findAll(new PageRequest(0, 0));
+	 * 
+	 * }
+	 */
 
 }
--- BACK/AMICOServer/src/main/java/com/example/demo/user/UserRepository.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/user/UserRepository.java
@@ -3,16 +3,15 @@ package com.example.demo.user;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.example.demo.course.Course;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 	User findByUserID(long id);
+
 	User findByUsername(String name);
+
 	User findByInternalName(String name);
+
 	User findByUserMail(String userMail);
-	List<User> findByIsStudent(boolean isStudend);
+
+	List<User> findByIsStudent(boolean isStudent);
 }
