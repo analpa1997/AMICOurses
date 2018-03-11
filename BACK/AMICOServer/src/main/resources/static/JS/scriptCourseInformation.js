ae0f69cf8353c3d6d169dacf7a6541f69d999fb1
mest96 <mestilles96@gmail.com>
2018-03-11 12:37:21 +0100

    courseInformationRestController

--- BACK/AMICOServer/src/main/resources/static/JS/scriptCourseInformation.js
+++ BACK/AMICOServer/src/main/resources/static/JS/scriptCourseInformation.js
@@ -2,7 +2,7 @@
  * 
  */
 
-var urlSearch = '/api/courseInformation/';
+var urlSearch = 'localhost:8080/api/courseInformation/';
 var name = $('#textSearch').val();
 	
 $('document').ready(function(){
@@ -11,13 +11,15 @@ name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
 		
 $.ajax({
 	type: 'GET',
+	contentType: "application/json",
 	url: urlSearch + "&name=" + name,
-	success: function(data){
-		$("courseDescription").append(
-			"<p>"+data.items[i].course.name+"</p>") + 
-		    "<p>"+data.items[i].course.courseDescription+"</p>") +
-            "<p>"+data.items[i].course.startDateString+"</p>") +
-		   "<p>"+data.items[i].course.endDateString+"</p>");
+	dataType: 'json',
+	success: function(data) {
+		$("body").append(
+				"<p>" +  data.course.internalName + "</p>" +
+				"<p>" +  data.course.courseDescription + "</p>" +
+				"<p>" +  data.course.startDateString + "</p>" +
+				"<p>" +  data.course.endDateString + "</p>");
 	},
 	error: function(exception){alert(exception.message);}
 	})
@@ -25,16 +27,17 @@ $.ajax({
 	
 function showSkills(){
 		
-		var name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
+		name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
 		
 		$.ajax({
 			type: 'GET',
-			url: urlSearch "&name=" + name + "/Skills",
-			success: function(data){
+			url: urlSearch + "&name=" + name + "/Skills",
+			success: function(data) {
 				for(var i=0; i<data.items.length; i++){
-					$("courseSkills").append(
-					"<p>"+data.items[i].skillsList[i].skillName+"</p>") +
-					"<p>"+data.items[i].skillsList[i].skillDescription+"</p>");
+					$("body").append(
+					'<p>'+data.items[i].skillsList[i].skillName+'</p>' +
+					'<p>'+data.items[i].skillsList[i].skillDescription+'</p>');
+			};
 			},
 			error: function(exception){console.log('ERROR');}
 		})
@@ -42,16 +45,17 @@ function showSkills(){
 
 function showSubjects(){
 	
-	var name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
+	name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
 	
 	$.ajax({
 		type: 'GET',
-		url: urlSearch "&name=" + name + "/Subjects",
+		url: urlSearch + "&name=" + name + "/Subjects",
 		success: function(data){
 			for(var i=0; i<data.items.length; i++){
-				$("courseSubjects").append(
-				"<p>"+data.items[i].subjectsList[i].name+"</p>") +
-				"<p>"+data.items[i].skillsList[i].description+"</p>");
+				$("body").append(
+				'<p>'+data.items[i].subjectsList[i].name+'<p>' +
+				'<p>'+data.items[i].skillsList[i].description+'<p>');
+		};
 		},
 		error: function(exception){console.log('ERROR');}
 	})
@@ -59,11 +63,11 @@ function showSubjects(){
 
 function showCourseAdded(){
 	
-	var name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
+	name = $('#textSearch').val().replace(new RegExp(" ", 'g'), "-").toLowerCase();
 	
 	$.ajax({
 		type: 'GET',
-		url: urlSearch "&name=" + name + "/add",
+		url: urlSearch + "&name=" + name + "/add",
 		success: function(data){
 			;
 		},
