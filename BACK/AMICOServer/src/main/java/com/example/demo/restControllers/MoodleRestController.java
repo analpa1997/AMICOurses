69ecda6c11a98c65f8614ed663923377a909ae37
mesteban96 <mestilles96@gmail.com>
2018-03-18 18:50:59 +0100

    DockerFolder

--- BACK/AMICOServer/src/main/java/com/example/demo/restControllers/MoodleRestController.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/restControllers/MoodleRestController.java
@@ -1,7 +1,5 @@
 package com.example.demo.restControllers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
