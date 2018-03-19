b961f53aafe612b0432ca4ab160d0ec1719dda14
analpa1997 <a.alcojor@alumnos.urjc.es>
2018-03-19 11:46:34 +0100

    Fixed null icon Study Item

--- BACK/AMICOServer/src/main/java/com/example/demo/studyItem/StudyItem.java
+++ BACK/AMICOServer/src/main/java/com/example/demo/studyItem/StudyItem.java
@@ -70,152 +70,147 @@ public class StudyItem {
 	public StudyItem() {
 	}
 
-	public StudyItem(String type, String name, Integer module, String filename, String originalName) {
-		setIcon(type);
-		if (!type.isEmpty()) {
-			type = "-" + type;
-		}
-		this.type = "file" + type;
-		this.name = name;
-		this.module = module;
-		this.fileName = filename;
-		this.originalName = originalName;
-		this.isPractice = false;
+	public StudyItem(String type, String name) {
+		this(type, name, 0, "", "");
 	}
 
 	public StudyItem(String type, String name, Integer module, String originalName) {
 		this(type, name, module, "", originalName);
 	}
-	
-	public StudyItem(String type, String name) {
-		this(type, name, 0, "", "");
+
+	public StudyItem(String type, String name, Integer module, String filename, String originalName) {
+		if (type == null)
+			type = "";
+		setIcon(type);
+		if (!type.isEmpty())
+			type = "-" + type;
+		this.type = "file" + type;
+		this.name = name;
+		this.module = module;
+		fileName = filename;
+		this.originalName = originalName;
+		isPractice = false;
+
 	}
 
 	/* Methods */
 
-	public long getStudyItemID() {
-		return studyItemID;
-	}
-
-	public void setStudyItemID(long studyItemID) {
-		this.studyItemID = studyItemID;
-	}
-
-	public String getType() {
-		return type;
-	}
-
-	public void setType(String type) {
-		if (!type.isEmpty()) {
-			type = "-" + type;
-		}
-		this.type = "file" + type;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Subject getSubject() {
-		return subject;
-	}
-
-	public void setSubject(Subject subject) {
-		this.subject = subject;
-	}
-
-	public int getModule() {
-		return module;
-	}
-
-	public void setModule(int module) {
-		this.module = module;
-	}
-
-	public String getFileName() {
-		return fileName;
-	}
-
-	public void setFileName(String fileName) {
-		this.fileName = fileName;
-	}
-
-	public String getOriginalName() {
-		return originalName;
-	}
-
-	public void setOriginalName(String originalName) {
-		this.originalName = originalName;
-	}
-
-	public String getExtension() {
-		return extension;
-	}
-
-	public void setExtension(String extension) {
-		this.extension = extension;
-	}
-
-	public String getIcon() {
-		return icon;
-	}
-
-	public void setIcon(String icon) {
-		this.icon = icon;
-	}
-
-	public boolean isPractice() {
-		return isPractice;
-	}
-
-	public void setPractice(boolean isPractice) {
-		this.isPractice = isPractice;
-	}
-
-	public List<Practices> getPractices() {
-		return practices;
-	}
-
-	public void setPractices(List<Practices> practices) {
-		this.practices = practices;
-	}
-
 	public void copy(StudyItem origin) {
 
-		if (origin.name != null && !origin.name.isEmpty()) {
-			this.name = origin.name;
-		}
-		if (origin.fileName != null && !origin.fileName.isEmpty()) {
-			this.fileName = origin.fileName;
-		}
-		if (origin.module != null) {
-			this.module = origin.module;
-		}
-		if (origin.originalName != null && !origin.originalName.isEmpty()) {
-			this.originalName = origin.originalName;
-		}
-		if (origin.extension != null && !origin.extension.isEmpty()) {
-			this.extension = origin.extension;
-		}
+		if (origin.name != null && !origin.name.isEmpty())
+			name = origin.name;
+		if (origin.fileName != null && !origin.fileName.isEmpty())
+			fileName = origin.fileName;
+		if (origin.module != null)
+			module = origin.module;
+		if (origin.originalName != null && !origin.originalName.isEmpty())
+			originalName = origin.originalName;
+		if (origin.extension != null && !origin.extension.isEmpty())
+			extension = origin.extension;
 		if (origin.icon != null && !origin.icon.isEmpty()) {
-			this.icon = origin.icon;
-			this.type = "file-" + icon;
+			icon = origin.icon;
+			type = "file-" + icon;
 		}
-		if (origin.practices != null && !origin.practices.isEmpty()) {
-			this.practices = origin.practices;
-		}
+		if (origin.practices != null && !origin.practices.isEmpty())
+			practices = origin.practices;
 	}
 
 	@Override
 	public boolean equals(Object obj2) {
 		boolean sameObj = false;
 		if (obj2 != null && obj2 instanceof StudyItem)
-			sameObj = this.studyItemID == ((StudyItem) obj2).studyItemID;
+			sameObj = studyItemID == ((StudyItem) obj2).studyItemID;
 		return sameObj;
+	}
+
+	public String getExtension() {
+		return extension;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public int getModule() {
+		return module;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getOriginalName() {
+		return originalName;
+	}
+
+	public List<Practices> getPractices() {
+		return practices;
+	}
+
+	public long getStudyItemID() {
+		return studyItemID;
+	}
+
+	public Subject getSubject() {
+		return subject;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public boolean isPractice() {
+		return isPractice;
+	}
+
+	public void setExtension(String extension) {
+		this.extension = extension;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public void setModule(int module) {
+		this.module = module;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setOriginalName(String originalName) {
+		this.originalName = originalName;
+	}
+
+	public void setPractice(boolean isPractice) {
+		this.isPractice = isPractice;
+	}
+
+	public void setPractices(List<Practices> practices) {
+		this.practices = practices;
+	}
+
+	public void setStudyItemID(long studyItemID) {
+		this.studyItemID = studyItemID;
+	}
+
+	public void setSubject(Subject subject) {
+		this.subject = subject;
+	}
+
+	public void setType(String type) {
+		if (!type.isEmpty())
+			type = "-" + type;
+		this.type = "file" + type;
 	}
 
 }
