1a22da1909de34acb3ae54669908b2fb62ae267e
mesteban96 <mestilles96@gmail.com>
2018-03-19 09:17:46 +0100

    pom.xml

--- BACK/AMICOServer/docker/create_image.bat
+++ BACK/AMICOServer/docker/create_image.bat
@@ -5,5 +5,5 @@ echo Cleaning folder and packaging app
 call mvnw clean package
 
 echo building docker image
-call docker build -f docker/Dockerfile -t mesteban96/amicoweb_sin .
+call docker build -f docker/Dockerfile -t mesteban96/amicoweb .
 echo Done
